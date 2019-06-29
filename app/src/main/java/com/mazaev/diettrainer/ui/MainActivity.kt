26cd128ae8de24eab7cb4ba2f903bf99d4de527f
mazaevav <mazaevav@altarix.ru>
2019-06-29 20:21:00 +0400

    Initial commit

---
+++ app/src/main/java/com/mazaev/diettrainer/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mazaev.diettrainer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mazaev.diettrainer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
